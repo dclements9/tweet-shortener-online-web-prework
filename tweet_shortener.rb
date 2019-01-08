003605dc745c026f336674cc37509cceeafb6913
Dylan Clements <dclements@protonmail.com>
2019-01-08 15:17:44 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -19,3 +19,5 @@ def word_substituter(tweet_string)
   tweet_array = tweet_string.to_a
   binding.pry
 end
+
+word_substituter("Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!")
