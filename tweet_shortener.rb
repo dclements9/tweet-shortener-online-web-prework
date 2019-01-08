92767296026f8a9731d9b16d0975260ad086fbe8
Dylan Clements <dclements@protonmail.com>
2019-01-08 15:52:44 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -34,6 +34,10 @@ def bulk_tweet_shortener(tweets_array)
   end
 end
 
-def selective_tweet_shortener
- 
+def selective_tweet_shortener (tweet)
+  if tweet.size >= 140
+    word_substituter(tweet)
+  else
+    tweet
+ end
 end
